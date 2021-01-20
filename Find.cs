2e007499019b9c337719d4b7fa6d449498dffcc3
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-20 08:58:44 +0100

    Add project files.

---
+++ Find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bflibcs
+{
+    public class Find
+    {
+        public static int LargestOfThree(int a, int b, int c)
+        {
+            int large;
+
+            if (a > b && a > c)
+                large = a;
+            else if (b > a && b > c)
+                large = b;
+            else
+                large = c;
+
+            return large;
+        }
+    }
+}
